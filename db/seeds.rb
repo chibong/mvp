b386f69ee4c2576d45a08ff59c2ab0fb4a6953be
Eric Chang <echang2013@kellogg.northwestern.edu>
2013-06-12 01:15:46 -0500

    updated formating

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,12 @@
+User.destroy_all
 
-data = [{name: "Eric Chang", password: "password"}]
+data = [{name: "Eric Chang", password: "password", question:"How am I doing as a leader?"}, {name: "Kara Chang", password: "password", question:"How am I doing as a leader?"}]
 
 data.each do |user_info|
   m = User.new
   m.name = user_info[:name ]
-  m.password_digest = user_info[:passwrod]
+  m.password_digest = user_info[:password]
+  m.user.question =[:question]
   m.save
 end
-
+puts "There are now #{User.count} users in the database"
